0b6559f0657abc708c827d1076e8a1771de82ac7
Lucas Amsellem <lucas.amsellem21@gmail.com>
2025-12-21 10:30:50 +0100

    feat(backend): installed bcrypt and added role in User schema

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,12 +1,21 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    role: { type: String, enum: ['admin', 'user'], default: 'user' },
   },
   { timestamps: true }
 );
+
+//pre("save") signifie : exécute cette fonction juste avant User.create() ou user.save()
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
+
+  this.password = await bcrypt.hash(this.password, 10);
+});
 
 export const User = mongoose.model('User', userSchema);
