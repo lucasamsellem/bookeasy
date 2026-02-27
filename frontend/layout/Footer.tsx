5b57ae008c1dcf698b5d35735ba25f73d7568709
Lucas Amsellem <lucas.amsellem21@gmail.com>
2026-02-27 14:51:04 +0100

    feat(front): improved Footer by adding fake social media links

--- frontend/layout/Footer.tsx
+++ frontend/layout/Footer.tsx
@@ -1,7 +1,44 @@
 export default function Footer() {
   return (
-    <footer className='text-center' style={{ padding: '1rem', borderTop: '1px solid #ddd' }}>
-      @2023 BookEasy - All rights reserved
+    <footer className='text-center py-6 border-t border-gray-300 bg-gray-50'>
+      <p className='mb-2 text-sm text-gray-600'>&copy; 2023 BookEasy - All rights reserved</p>
+
+      <div className='flex justify-center gap-4 mb-2'>
+        <a
+          href='https://github.com/lucasamsellem'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='text-gray-500 hover:text-gray-900 transition'
+        >
+          GitHub
+        </a>
+
+        <a
+          href='https://www.google.com'
+          target='_blank'
+          className='text-gray-500 hover:text-gray-900 transition'
+        >
+          Newsletter
+        </a>
+        <a
+          href='https://www.google.com'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='text-gray-500 hover:text-gray-900 transition'
+        >
+          Twitter
+        </a>
+        <a
+          href='https://www.google.com'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='text-gray-500 hover:text-gray-900 transition'
+        >
+          LinkedIn
+        </a>
+      </div>
+
+      <p className='text-xs text-gray-400'>Designed with ❤️ by BookEasy Team</p>
     </footer>
   );
 }
