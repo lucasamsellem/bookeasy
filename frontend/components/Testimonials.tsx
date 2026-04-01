a634ddec8217afcc57c478888536ae1c65e6f2f3
Lucas Amsellem <lucas.amsellem21@gmail.com>
2026-04-01 13:35:36 +0200

    feat(SCSS): Testimonials component is now styled with SCSS

--- frontend/components/Testimonials.tsx
+++ frontend/components/Testimonials.tsx
@@ -1,4 +1,5 @@
 import Avatar from './Avatar';
+import styles from './Testimonials.module.scss';
 
 const testimonials = [
   {
@@ -20,25 +21,20 @@ const testimonials = [
 
 export default function Testimonials() {
   return (
-    <section className='w-full bg-linear-to-b from-white to-slate-50 px-4 py-24 md:px-20'>
-      <h2 className='text-4xl font-bold text-slate-900  text-center mb-8'>
-        Ce que nos utilisateurs disent
-      </h2>
-      <div className='grid gap-6 md:grid-cols-3'>
+    <section className={styles.testimonials}>
+      <h2>Ce que nos utilisateurs disent</h2>
+      <div className={styles['grid-container']}>
         {testimonials.map((t, i) => (
-          <div
-            key={i}
-            className='bg-white shadow-lg rounded-xl p-6 flex flex-col items-start transition-transform'
-          >
-            <div className='grid grid-cols-2 items-center gap-x-2 mb-2'>
+          <div key={i} className={styles['testimonial-card']}>
+            <div className={styles.header}>
               <Avatar id={i} size={80} />
-              <div className='flex flex-col text-xl'>
-                <span className='font-semibold'>{t.name}</span>
-                <span className='text-sm text-gray-500'>{t.role}</span>
+              <div className={styles.info}>
+                <span className={styles.name}>{t.name}</span>
+                <span className={styles.role}>{t.role}</span>
               </div>
             </div>
 
-            <p className='text-gray-700 mb-4 border-t border-gray-200 pt-4'>“{t.text}”</p>
+            <p className={styles.text}>“{t.text}”</p>
           </div>
         ))}
       </div>
