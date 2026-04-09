aecdc2284788b4e9c13ebe5569ed283ea04165e1
Lucas Amsellem <lucas.amsellem21@gmail.com>
2026-04-10 00:02:07 +0200

    fix(deployment): fixed server creation

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,9 @@
-import express from 'express';
+import { app } from './app';
 import { connectDB } from './config/db';
 
 async function startServer() {
   try {
     await connectDB();
-
-    const app = express();
-    app.get('/', (req, res) => res.send('Server is running'));
 
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
