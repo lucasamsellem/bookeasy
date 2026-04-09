9347fb5963b150d70aa593bcd0dd4eb16646f29d
Lucas Amsellem <lucas.amsellem21@gmail.com>
2026-04-09 23:03:27 +0200

    fix(deployment): added uri for DB connection

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,14 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT || 3306), // corrigé
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  uri: process.env.DATABASE_URL,
 });
 
 export async function connectDB() {
