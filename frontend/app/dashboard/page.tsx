3fa5f96be9ca9f1fe6d639cf45e4578556280cfb
Lucas Amsellem <lucas.amsellem21@gmail.com>
2026-04-10 11:07:33 +0200

    fix(deployment): fixed /dashboard acess

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,20 +1,16 @@
+'use client';
+
 import AllBookings from '@/features/bookings/components/AllBookings';
 import AllReviews from '@/features/reviews/components/AllReviews';
 import UsersTable from '@/features/users/components/UsersTable';
-import { cookies } from 'next/headers';
+import { useUser } from '@/store/useUser';
 import { redirect } from 'next/navigation';
 
-export default async function DashboardPage() {
-  const cookieStore = await cookies();
-  const token = cookieStore.get('token')?.value;
+export default function DashboardPage() {
+  const { user } = useUser();
 
-  if (!token) redirect('/login');
-
-  const payload = JSON.parse(atob(token.split('.')[1]));
-
-  if (payload.role !== 'superAdmin') {
-    redirect('/');
-  }
+  if (!user) redirect('/login');
+  if (user.role !== 'superAdmin') redirect('/');
 
   return (
     <div className='space-y-10 p-10'>
